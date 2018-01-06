387e8c6a7e1221101553eb24c53e6b3b53e92338
chris <christianfritz.mail@gmail.com>
2018-01-06 01:03:39 +0100

    First routine

--- app.py
+++ app.py
@@ -4,10 +4,53 @@ import time
 from myDeezerApp import *
 
 
+app = None
+current_url = None
+last_url = None
+
+def get_RFID_content():
+    return "dzmedia:///track/10287076"
+
 def add_input(input_queue):
     while True:
-        user_input = sys.stdin.readline()
-        input_queue.put(user_input)
+        """
+        MUSIC CONTROL OVER RFID-CHIP
+        """
+
+        RFID = get_RFID_content()
+
+        if RFID is not None:
+            # An RFID-Card is inserted
+
+            if RFID != current_url:
+                # A RFID-Chip was inserted; set as new URL to play!
+                current_url = RFID
+
+                # Check if it is a new one (then set new content) or if chip was reinserted (then: continue play)
+                if RFID == last_url:
+                    # Continue playing
+                    print("Continue Song")
+                    input_queue.put("P")
+                else:
+                    # Entirely new Chip; Start new Song
+                    print("Start New Song")
+                    app.set_content(RFID)
+
+        else:
+            # No RFID-Card is inserted; Check the last Status of current_url
+
+            if current_url is not None:
+                # Issue Command to Pause music and set current_url to None
+                print("Pause Playing")
+                input_queue.put("P")
+                last_url = current_url
+                current_url = None
+
+        #user_input = sys.stdin.readline()
+
+        """
+        OTHER BUTTON CONTROL
+        """
 
 
 def main():
@@ -43,7 +86,7 @@ def main():
                 app.process_command(command)
 
     # Übergabe der URL, die abgespielt werden soll.
-    app.set_content(sys.argv[1])
+    #app.set_content(sys.argv[1])
 
     return 0
 
